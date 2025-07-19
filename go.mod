7b6fe52e6cda20f558e3faba77fecaa8c6b4d0fe
Hyperion147 <supernoob7903@gmail.com>
2025-07-19 21:06:24 +0530

    finished auth.go for signin and signup

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.4
 require gorm.io/gorm v1.30.0
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.5 // indirect
--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/Hyperion147/gometaverse/internal/auth"
 	"github.com/Hyperion147/gometaverse/internal/database"
 	"github.com/Hyperion147/gometaverse/internal/models"
 	"golang.org/x/crypto/bcrypt"
@@ -18,11 +19,11 @@ func NewAuthHandler() *AuthHandler {
 	return &AuthHandler{db: database.DB}
 }
 
-func (h *AuthHandler) SignUp (w http.ResponseWriter, r *http.Request) {
+func (h *AuthHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 	var req struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
-		Type string `json:"type"`
+		Type     string `json:"type"`
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
@@ -39,7 +40,7 @@ func (h *AuthHandler) SignUp (w http.ResponseWriter, r *http.Request) {
 	user := models.User{
 		Username: req.Username,
 		Password: string(hashedPassword),
-		Role: req.Type,
+		Role:     req.Type,
 		AvatarId: 1,
 	}
 
@@ -53,5 +54,42 @@ func (h *AuthHandler) SignUp (w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "User created successfully!",
+	})
+
 }
 
+func (h *AuthHandler) SignIn(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var user models.User
+	result := h.db.Where("username = ?", req.Username).First(&user)
+	if result.Error != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return 
+	}
+
+	token, err := auth.GenerateToken(int(user.ID), user.Username, user.Role)
+	if err != nil {
+		http.Error(w, "Error generating token", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+
+}
